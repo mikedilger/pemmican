89a0af63dc7fdb1d49b0399bb2de43a3f18e4660
Mike Dilger <mike@optcomp.nz>
2017-07-17 14:11:32 +1200

    Expose Config fields (oops!)

--- src/lib.rs
+++ src/lib.rs
@@ -25,16 +25,16 @@ pub struct Config {
     /// Number of threads for the CpuPool.  Note that handler functions are run on the
     /// main thread, and you must use `pemmican.pool` if you want to run code on a
     /// separate thread in the Pemmican CpuPool.  Defaults to 4.
-    num_threads: usize,
+    pub num_threads: usize,
 
     /// Configure the amount of time the server will wait for a "graceful shutdown".
     /// This is the amount of time after the shutdown signal is received the server
     /// will wait for all pending connections to finish. If the timeout elapses then
     /// the server will be forcibly shut down.  Defaults to 1s.
-    shutdown_timeout: Duration,
+    pub shutdown_timeout: Duration,
 
     /// Enable or disable Keep-alive.  Default is true.
-    keep_alive: bool,
+    pub keep_alive: bool,
 }
 
 impl Default for Config {
