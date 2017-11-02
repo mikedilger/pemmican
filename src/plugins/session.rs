9946d5b47b1252d811359da30df851b6be74705e
Mike Dilger <mike@optcomp.nz>
2017-11-02 15:57:01 +1300

    Session: only manage the cookie, not associated data:
    This is because in order to access associated data, the consumer of the library
    would still need to store an Arc to this plugin in their shared state. If they
    are doing that, they could easily create a CHashMap there themselves, and there
    would be less complication for them.  They also now have the flexibility to use
    any other state storage mechanism they choose.

--- src/plugins/session.rs
+++ src/plugins/session.rs
@@ -1,6 +1,5 @@
 
 use futures::Future;
-use chashmap::{CHashMap, ReadGuard, WriteGuard};
 use plugins::{Plugin, PluginData};
 use hyper::header::Cookie as CookieHeader;
 use hyper::header::SetCookie;
@@ -11,16 +10,19 @@ use textnonce::TextNonce;
 /// earlier requests. Sessions are maintained automatically by always setting a
 /// cookie initially and finding it again on subsequent requests.
 ///
+/// This plugin only manages the cookie and maintains PluginData.session_id.
+/// Associating data with that session_id is left up to the consumer of this library
+/// (hint: Store it in your shared state, the S type parameter on Pemmican, perhaps
+/// with a CHashMap)
+///
 /// Plug this in before main content handling plugins
-pub struct Session<D: Default> {
+pub struct Session {
     cookie_name: String,
-    data: CHashMap<String, D>,
     secure: bool,
     http_only: bool,
 }
 
-impl<D> Session<D>
-    where D: Default
+impl Session
 {
     /// Create the Session plugin.
     ///
@@ -30,30 +32,17 @@ impl<D> Session<D>
     ///
     /// `http_only` is whether or not to restrict the cookie to the HTTP protocol (or else
     /// allow javascript to access it)
-    pub fn new(cookie_name: String, secure: bool, http_only: bool) -> Session<D> {
+    pub fn new(cookie_name: String, secure: bool, http_only: bool) -> Session {
         Session {
             cookie_name: cookie_name,
-            data: CHashMap::new(),
             secure: secure,
             http_only: http_only,
         }
     }
-
-    /// Get a reference to a session's data D.  Use `PluginData`s session_id for the
-    /// current session (if any).
-    pub fn get_ref<'a>(&'a self, session_id: &String) -> Option<ReadGuard<'a, String, D>> {
-        self.data.get(session_id)
-    }
-
-    /// Get a mutable reference to a session's data D.  Use `PluginData`s session_id
-    /// for the current session (if any).
-    pub fn get_mut<'a>(&'a self, session_id: &String) -> Option<WriteGuard<'a, String, D>> {
-        self.data.get_mut(session_id)
-    }
 }
 
-impl<S,E,D> Plugin<S,E> for Session<D>
-    where S: 'static, E: 'static, D: Default
+impl<S,E> Plugin<S,E> for Session
+    where S: 'static, E: 'static
 {
     fn handle(&self, mut data: PluginData<S>)
               -> Box<Future<Item = PluginData<S>, Error = E>>
@@ -66,19 +55,14 @@ impl<S,E,D> Plugin<S,E> for Session<D>
         }
 
         if let Some(key) = maybe_key {
-            if self.data.contains_key(&key) {
-                // Associate existing session
-                data.session_id = Some(key.to_owned());
-                return Box::new(::futures::future::ok(data));
-            }
+            // Associate existing session
+            data.session_id = Some(key.to_owned());
+            return Box::new(::futures::future::ok(data));
         }
 
         // Create new session
         let key = TextNonce::new().into_string();
         data.session_id = Some(key.clone());
-
-        // Create new session data
-        self.data.insert(key.clone(), D::default());
 
         // Create the cookie
         let mut cookie = Cookie::new(self.cookie_name.clone(), key);
